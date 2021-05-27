073530bff6765ff4cdbbcf9add932b2d85b4e3d3
NeroTsk <thiagosak@live.com>
2021-05-27 12:50:01 -0300

    First up

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { Header } from "../components/Header";
+// SPA => exemplo /* 
+//useEffect(() => {
+//  fetch('http://localhost:3333/episodes')
+//  .then(response => response.json())
+//  .then(data => console.log(data))
+//}, [])
+
+// SSR
+// export async function getServerSideProps(){
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json();
+
+//   return{
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+// SSG
+
+
+export default function Home(props) {
+  console.log(props.episodes)
+
+  return (
+   <h1>Index</h1>
+  )
+}
+
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return{
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
